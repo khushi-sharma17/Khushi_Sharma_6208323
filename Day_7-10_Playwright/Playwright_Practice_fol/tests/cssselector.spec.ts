faefc1c05bee15719b2b00a5346bba5f9c74dfb6
khushi-gunnu17 <khushiastrogeek@gmail.com>
2026-02-28 15:23:04 +0530

    tasks

--- Day_7-10_Playwright/Playwright_Practice_fol/tests/cssselector.spec.ts
+++ Day_7-10_Playwright/Playwright_Practice_fol/tests/cssselector.spec.ts
@@ -1,24 +1,59 @@
 import {test} from "@playwright/test"
 
 
+
 test("css selector", async({page}) => {
 
-    // await page.goto('https://practicetestautomation.com/practice-test-login/')
+    await page.goto(`https://www.amazon.in/`)
 
-    // await page.locator("input#username").fill("student")
+    await page.locator(`//input[@id=twotabsearchtextbox]`).fill("Samsung Phones")
+    await page.locator(`//input[@id="nav-search-submit-button"]`).click()
+    // await page.locator(`xpath=span[@class="a-price-whole(1)"]`).last().click()
+    // await page.locator(`xpath=span[@class="a-price-whole"]`).last().click()
+    await page.locator(`//span[@class="a-price-whole"]`).nth(2).innerText()
 
-    // await page.locator("input#password").fill("Password123")
-
-    // await page.locator("button#submit").click()
-
-    // await page.locator(".wp-block-button__link.has-text-color.has-background.has-very-dark-gray-background-color").click()
-
-
-    await page.goto(`https://techbeamers.com/practice-test-login/`)
-
-    await page.locator(`input#username`).fill("username")
-
+    // await page.locator('//input[@type="submit"]').click();
 })
+
+
+
+
+// test("css selector", async({page}) => {
+
+//     await page.goto(`https://www.amazon.com`)
+
+//     await page.locator(`xpath=/html/body/div/header/div/div/div[2]/div/form/div[2]/div/input`).click()
+
+//     // await page.locator('//input[@type="submit"]').click();
+// })
+
+
+
+
+// test("css selector", async({page}) => {
+
+//     // await page.goto('https://practicetestautomation.com/practice-test-login/')
+
+//     // await page.locator("input#username").fill("student")
+
+//     // await page.locator("input#password").fill("Password123")
+
+//     // await page.locator("button#submit").click()
+
+//     // await page.locator(".wp-block-button__link.has-text-color.has-background.has-very-dark-gray-background-color").click()
+
+
+//     await page.goto(`https://techbeamers.com/practice-test-login/`)
+
+//     await page.locator(`input#username`).fill("username")
+
+// })
+
+
+
+
+
+
 
 // do the above code also with xml structure
 
